a45a49a27b1be336f2379432fb05f0f33c94ab95
Eriikn <75541439+Eriikn@users.noreply.github.com>
2020-12-11 21:18:04 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // using module.exports here so gatsby-config can read it
 module.exports = {
   // TODO: uncomment next line and enter your own public API key
-  // builderAPIKey: '<YOUR PUBLIC API KEU>'
+  // builderAPIKey: '<71ff2ffa29b449f9b447815e7d3c868b>'
 };
